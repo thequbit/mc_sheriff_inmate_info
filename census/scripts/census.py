9de9b41694e29ce48f3bd7273f4420b2997318c1
thequbit <thequbit@gmail.com>
2013-07-23 01:48:22 -0400

    switched back to non-debug code base

--- census/scripts/census.py
+++ census/scripts/census.py
@@ -4,10 +4,10 @@ from parseinmates import parseinmates
 
 def main():
     print "pulling todays census."
-    #pdftext,success = pullpdf()
-    success = True
-    with open("pdftext.txt", "r") as _file:
-        pdftext = _file.read()
+    pdftext,success = pullpdf()
+    #success = True
+    #with open("pdftext.txt", "r") as _file:
+    #    pdftext = _file.read()
     if success:
         print "pulling inmates from census data."
         _inmates,success = pullinmates(pdftext)
