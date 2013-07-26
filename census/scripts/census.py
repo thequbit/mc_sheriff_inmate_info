b2abe5afcf2993c9071bd217212d0cfbe1798b3d
thequbit <thequbit@gmail.com>
2013-07-25 23:57:55 -0400

    rearch'd to support mysql db access layer rather than sqlite3

--- census/scripts/census.py
+++ census/scripts/census.py
@@ -1,118 +1,49 @@
-from pullpdf import pullpdf
-from pullinmates import pullinmates
-from parseinmates import parseinmates
+import datetime
 
-import sqlite3 as lite
+# Helper files
+from getinmates import getinmates
+
+# DB access layer 
+from scraperruns import scraperruns
+from inmates import inmates
+from custodies import custodies
+from judges import judges
+from courts import courts
+from arrestingagencies import arrestingagencies
+from arresttypes import arresttypes
+from charges import charges
+from bookings import bookings
+
+
 
 def main(DEBUG=False):
-    print "pulling todays census."
-    if DEBUG == True:
-        success = True
-        with open("pdftext.txt", "r") as _file:
-            pdftext = _file.read()
+    print "Pulling inmates from the interwebs ..."
+
+    theinmates,success = getinmates(DEBUG)
+    if success:
+        print "Imates pulled successfully, pushing to database."
     else:
-        pdftext,success = pullpdf()
-    if success:
-        print "pulling inmates from census data."
-        _inmates,success = pullinmates(pdftext)
-    if success:
-        print "parsing booking information for {0} inmates ...".format(len(_inmates))
-        inmates,success = parseinmates(_inmates)
-    if success:
-        print "{0} inmates processed successfully.".format(len(inmates))
-    if success:
-        print "Creating SQLite3 database ..."
-        con = lite.connect('inmates.db')
+        raise Exception("Something bad happened ... no inmates were pulled.")
 
-        with con:
-            cur = con.cursor()
+    print "Pushing {0} inmates to the database ...".format(len(theinmates))
 
-            q = "PRAGMA foreign_keys = ON"
-            cur.execute(q)
-
-            q = ("CREATE TABLE inmates("
-            "inmateid INTEGER PRIMARY KEY, "
-            "name TEXT, "
-            "number INTEGER, "
-            "sex TEXT, "
-            "race TEXT, "
-            "dob TEXT, "
-            "custodydate TEXT, "
-            "custodytime TEXT, "
-            "custodyclass TEXT"
-            ")")
-            cur.execute(q)
-    
-            q = ("CREATE TABLE bookings("
-            "bookingid INTEGER PRIMARY KEY, "
-            "inmateid INTEGER, "
-            "bookdate TEXT, "
-            "booktype TEXT, "
-            "custodytype TEXT, "
-            "bail TEXT, "
-            "bond TEXT, "
-            "court TEXT, "
-            "judge TEXT, "
-            "arrestingagency TEXT, "
-            "arrestype TEXT, "
-            "roc TEXT, "
-            "charge TEXT, "
-            "indict TEXT, "
-            "adjusteddate TEXT, "
-            "term TEXT, "
-            "FOREIGN KEY(inmateid) REFERENCES inmates(inmateid)"
-            ")")
-            cur.execute(q)
-    
-            print "Saving inmate data to SQLite3 database ..."
-    
-            inmatecount = 0
-            bookingcount = 0
-    
-            for inmate in inmates:
-                name,number,sex,race,dob,custodydate,custodytime,custodyclass,bookings = inmate
-    
-                q = ("INSERT INTO inmates("
-                "name,"
-                "number,"
-                "sex,"
-                "race,"
-                "dob,"
-                "custodydate,"
-                "custodytime,"
-                "custodyclass"
-                ") "
-                "VALUES(?,?,?,?,?,?,?,?)")
-                cur.execute(q,(name,number,sex,race,dob,custodydate,custodytime,custodyclass))
-    
-                q = "SELECT last_insert_rowid()"
-                cur.execute(q)
-                inmateid, = cur.fetchone()
-    
-                for booking in bookings:
-                    bookdate,booktype,custodytype,bail,bond,court,judge,arrestingagency,arresttype,roc,charge,indict,adjusteddate,term = booking
-                    q = ("INSERT INTO bookings("
-                    "inmateid,"
-                    "bookdate,"
-                    "booktype,"
-                    "custodytype,"
-                    "bail,"
-                    "bond,"
-                    "court,"
-                    "judge,"
-                    "arrestingagency,"
-                    "arrestype,"
-                    "roc,"
-                    "charge,"
-                    "indict,"
-                    "adjusteddate,"
-                    "term"
-                    ") "
-                    "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
-                    cur.execute(q,(inmateid,bookdate,booktype,custodytype,bail,bond,court,judge,arrestingagency,arresttype,roc,charge,indict,adjusteddate,term))
-                    bookingcount += 1
-                inmatecount += 1
-        print "Added {0} inmates and {1} bookings to the SQLite3 database.".format(inmatecount,bookingcount)
-    print "Exiting."
+    i = inmates()
+    for inmate in theinmates:
+        name,mcid,sex,race,dob,custodydate,custodytime,custodyclass,bookings = inmate
+        parts = name.split(',')
+        first = parts[0]
+        parts2 = parts[1].rsplit(' ',1)[0]
+        last = parts[1]
+        if len(parts2) != 0:
+            last = parts2[0]
+        middle = ""
+        if len(parts2) == 2:
+            middle = parts2[1]
+        try:
+            isodob = datetime.datetime.strptime(dob, '%m-%d-%Y').date().isoformat()
+        except:
+            print "name: {0}, dob: ".format(name,dob)
+            raise Exception("error.")
+        i.add(first,last,middle,mcid,sex,race,isodob)
 
 main()
